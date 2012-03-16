90645241ea590a78036b03a89c5dc6bb2d55d77b
Jake Trent <trent.jake@gmail.com>
2012-03-16 15:04:39 -0600

    swipe scroll per col

--- static/js/item/ItemView.js
+++ static/js/item/ItemView.js
@@ -1,4 +1,4 @@
-define(['tmpl!item/ItemView', 'tmpl!item/AddPointView'], function (itemViewTmpl, addPointViewTmpl) {
+define(['tmpl!item/ItemView', 'tmpl!item/AddPointView', 'vendor/jquery.touchwipe.1.1.1'], function (itemViewTmpl, addPointViewTmpl) {
   return Backbone.View.extend({
     tagName: 'li',
     className: 'item',
@@ -12,7 +12,26 @@ define(['tmpl!item/ItemView', 'tmpl!item/AddPointView'], function (itemViewTmpl,
     },
     render: function () {
       $(this.el).html(itemViewTmpl(this.model.toJSON())).append(addPointViewTmpl());
+      this.$('.slider').touchwipe({
+        wipeLeft: this.scrollToRight,
+        wipeRight: this.scrollToLeft
+      });
       return this;
+    },
+    scrollToRight: function () {
+      this.scroll(1);
+    },
+    scrollToLeft: function () {
+      this.scroll(-1);
+    },
+    scroll: function (direction) {
+      var itemWidth = $(this.el).width();
+      var extraWidthEach = 1; /*border*/
+      var itemIndex = $('.item').index($(this.el));
+      var startingPosX = itemIndex * (itemWidth + extraWidthEach);
+      var winWidth = $(window).width();
+      var itemsInWindow = Math.floor(winWidth / itemWidth); /*no border on single-col device scrn */
+      $('body').animate({ scrollLeft: (startingPosX + (direction * itemsInWindow * (itemWidth + extraWidthEach))) });
     },
     editTitle: function (evt) {
       Backbone.Events.trigger('edit', this.model.get('title'), this.saveTitle, $(evt.currentTarget).closest('.item'));
